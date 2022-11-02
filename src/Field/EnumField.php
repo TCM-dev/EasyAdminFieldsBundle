407dba9f446e69852783fa818b577d60aa2fd472
Valentin W <valentin.wojtasinski@gmail.com>
2022-11-02 21:20:09 +0100

    fix: enum field works with enum fqn only

--- src/Field/EnumField.php
+++ src/Field/EnumField.php
@@ -10,16 +10,18 @@ use Symfony\Component\Form\Extension\Core\Type\EnumType;
 
 class EnumField
 {
-    public static function new(string $propertyName, array $choices, string $pageName, $label = null): FieldInterface
+    public static function new(string $propertyName, string $enumFqn, string $pageName, $label = null): FieldInterface
     {
+        $enum = new \ReflectionEnum($enumFqn);
 
         $field = ChoiceField::new($propertyName, $label)
-            ->setChoices($choices)
-            ->setFormType(EnumType::class);
+            ->setChoices($enum->getCases())
+            ->setFormType(EnumType::class)
+            ->setFormTypeOption('class', $enumFqn);
 
         if (in_array($pageName, [Crud::PAGE_INDEX, Crud::PAGE_DETAIL], true)) {
             $field->setChoices(array_reduce(
-                $choices,
+                $enum->getCases(),
                 static fn(array $choices, $enum) => $choices + [$enum->name => $enum->value],
                 [],
             ));
