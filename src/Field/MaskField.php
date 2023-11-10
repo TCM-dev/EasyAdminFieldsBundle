0a9b98c421a090f4ba3615d7ea28595cbacec501
gboddaert <gboddaert@insitaction.com>
2023-11-10 14:13:09 +0100

    BC: maskField

--- src/Field/MaskField.php
+++ src/Field/MaskField.php
@@ -3,11 +3,17 @@
 namespace Insitaction\EasyAdminFieldsBundle\Field;
 
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MaskField
 {
-    public static function adapt(FieldInterface $field, array $map = []): FieldInterface
+    public static function adapt(FieldInterface $field, array $options = []): FieldInterface
     {
+
+        $resolver = new OptionsResolver();
+        self::configureOptions($resolver);
+        $options = $resolver->resolve($options);
+
         if (!method_exists($field, 'setFormTypeOptions')) {
             return $field;
         }
@@ -16,7 +22,8 @@ class MaskField
             ->setFormTypeOptions([
                 'row_attr' => [
                     'data-controller' => 'mask-field',
-                    'data-mask-field-map' => self::encodeMap($map)
+//                    'data-mask-field-map' => self::encodeMap($map),
+                    'data-mask-field-options' => json_encode($options, JSON_THROW_ON_ERROR),
                 ],
             ]);
     }
@@ -33,5 +40,15 @@ class MaskField
         }
 
         return json_encode($mapElementArray, JSON_THROW_ON_ERROR);
+    }
+
+
+    public static function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('identifier_type', 'value');
+        $resolver->setAllowedValues('identifier_type', ['label', 'value']);
+
+        $resolver->setRequired('map');
+        $resolver->setAllowedTypes('map', 'string[][]');
     }
 }
